b34cd8c6309046001e17630f1de681e9e783610f
Arturo Munoz <munoz.arturoroman@gmail.com>
2024-06-24 13:46:10 -0400

    build: add components for rendering content

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,37 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: true },
+    content: {
+        markdown: {
+            tags: {
+                a: "md-a",
+                blockquote: "md-blockquote",
+                code: "md-code",
+                "code-block": "md-code-block",
+                em: "md-em",
+                h1: "md-h1",
+                h2: "md-h2",
+                h3: "md-h3",
+                h4: "md-h4",
+                h5: "md-h5",
+                h6: "md-h6",
+                hr: "md-hr",
+                img: "md-img",
+                label: "md-label",
+                li: "md-li",
+                ol: "md-ol",
+                p: "md-p",
+                strong: "md-strong",
+                table: "md-table",
+                tbody: "md-tbody",
+                td: "md-td",
+                th: "md-th",
+                thead: "md-thead",
+                tr: "md-tr",
+                ul: "md-ul",
+            },
+        },
+    },
     components: {
         dirs: ["~/components", { path: "~/components/global", global: true }],
     },
