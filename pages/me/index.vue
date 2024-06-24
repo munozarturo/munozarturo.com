16b4d62f4e4f3b273d5712e531d114db316a52cb
Arturo Munoz <munoz.arturoroman@gmail.com>
2024-06-24 12:26:57 -0400

    build: sync

--- pages/me/index.vue
+++ pages/me/index.vue
@@ -1,10 +1,5 @@
 <template>
-    <div>
-        <Icon name="at" class="w-6" /><Icon name="github" class="w-6" /><Icon
-            name="linkedin"
-            class="w-6"
-        /><Icon name="share" class="w-6" />
-    </div>
+    <div></div>
 </template>
 
 <script lang="ts" setup></script>
