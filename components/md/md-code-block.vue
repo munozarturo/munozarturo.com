a995c9cf0c676fc6e9bd5aa0be05dc9b2d691a08
Arturo Munoz <munoz.arturoroman@gmail.com>
2024-06-24 18:15:34 -0400

    build: register md components

--- components/md/md-code-block.vue
+++ components/md/md-code-block.vue
@@ -1,8 +1,5 @@
 <template>
-    <div class="flex flex-col">
-        <div class="flex flex-row">lang</div>
-        <code class="font-mono bg-gray-100 px-1 py-0.5 rounded"><slot /></code>
-    </div>
+    <code class="font-mono bg-gray-100 px-1 py-0.5 rounded"><slot /></code>
 </template>
 
 <script setup lang="ts"></script>
--- pages/me/index.vue
+++ pages/me/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div><MdCodeBlock>Md Code Block</MdCodeBlock></div>
+    <div><MdA href="/hello">Hello Anchor</MdA></div>
 </template>
 
 <script lang="ts" setup></script>
