f8b7bffa1e9ca20ea35c443e12e6e73fcf44c378
Arturo Munoz <munoz.arturoroman@gmail.com>
2024-06-25 10:26:06 -0400

    build: added copy functionality

--- components/content/prose-pre.vue
+++ components/content/prose-pre.vue
@@ -1,16 +1,20 @@
 <template>
     <div class="flex flex-col mx-4">
         <div
-            class="w-full h-fit flex flex-row justify-between px-2 py-1 bg-primary rounded-t-md"
+            class="w-full h-fit flex flex-row items-center justify-between px-2 py-1 bg-primary rounded-t-md"
         >
             <p class="font-bold text-secondary">
                 {{ language }}
             </p>
-            <button class="font-bold text-secondary" @click="copy">
-                <Icon name="copy" class="w-full h-full stroke-current" />
+            <button
+                class="font-bold text-secondary flex items-center"
+                @click="copy"
+            >
+                <Icon name="copy" class="stroke-current" />
             </button>
         </div>
         <pre
+            ref="codeBlock"
             class="w-full h-fit font-mono p-2 rounded-b-md border overflow-x-auto"
         ><slot
         /></pre>
@@ -26,8 +30,18 @@ defineProps({
 });
 
 const toaster = useToasterStore();
+const codeBlock = ref<HTMLPreElement | null>(null);
 
 function copy(): void {
-    toaster.addMessage("Copied to clipboard!", "success");
+    if (codeBlock.value) {
+        try {
+            navigator.clipboard.writeText(codeBlock.value.textContent || "");
+            toaster.addMessage("Copied", "success");
+        } catch (e: any) {
+            toaster.addMessage("Failed to copy.", "error");
+        }
+    } else {
+        toaster.addMessage("Nothing to copy.", "error");
+    }
 }
 </script>
