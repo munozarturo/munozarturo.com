989be8f98523a644e55d70c2655255925272e7fe
Arturo Munoz <munoz.arturoroman@gmail.com>
2024-07-10 14:43:20 -0400

    build: add in progress

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,9 +1,12 @@
 <template>
-    <div>
-        {{ route.fullPath }}
+    <div class="w-1/2 flex flex-col items-start justify-center space-y-4 pt-60">
+        <h1
+            class="flex flex-row items-center justify-center text-5xl font-bold"
+        >
+            i'm still working on this bit.
+        </h1>
+        <div class="text-2xl pl-2">hopefully i'm done soon.</div>
     </div>
 </template>
 
-<script setup>
-const route = useRoute();
-</script>
+<script setup></script>
--- pages/me/index.vue
+++ pages/me/index.vue
@@ -1,5 +1,12 @@
 <template>
-    <div></div>
+    <div class="w-1/2 flex flex-col items-start justify-center space-y-4 pt-60">
+        <h1
+            class="flex flex-row items-center justify-center text-5xl font-bold"
+        >
+            i'm still working on this bit.
+        </h1>
+        <div class="text-2xl pl-2">hopefully i'm done soon.</div>
+    </div>
 </template>
 
-<script lang="ts" setup></script>
+<script setup></script>
