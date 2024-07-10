ec5db01a2c8dd024abfeffd6001273965c612d95
Arturo Munoz <munoz.arturoroman@gmail.com>
2024-07-10 14:59:21 -0400

    build: update project preview

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,9 +1,22 @@
 <template>
-    <div>
-        <h1>Projects</h1>
-        <ul>
+    <div class="w-full px-4 md:w-2/3 md:px-0 lg:w-1/2 flex flex-col py-8">
+        <ul class="space-y-6">
             <li v-for="project in projects" :key="project._path">
-                <NuxtLink :to="project._path">{{ project.title }}</NuxtLink>
+                <NuxtLink
+                    :to="project._path"
+                    class="w-full h-fit flex flex-col"
+                >
+                    <div class="flex flex-row items-end justify-between">
+                        <h1 class="text-4xl font-bold">{{ project.title }}</h1>
+                        <p>{{ formatDate(new Date(project.date)) }}</p>
+                    </div>
+                    <h2 class="text-2xl font-bold text-tertiary">
+                        {{ project.subtitle }}
+                    </h2>
+                    <div class="flex fex-row space-x-2 text-tertiary">
+                        <p v-for="tag in project.tags">#{{ tag }}</p>
+                    </div>
+                </NuxtLink>
             </li>
         </ul>
     </div>
