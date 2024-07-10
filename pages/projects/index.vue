69ddddc6a3d8bb702067d90f043c5d55145aca20
Arturo Munoz <munoz.arturoroman@gmail.com>
2024-07-10 15:06:30 -0400

    build: add select indicator

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,7 +1,16 @@
 <template>
     <div class="w-full px-4 md:w-2/3 md:px-0 lg:w-1/2 flex flex-col py-8">
         <ul class="space-y-6">
-            <li v-for="project in projects" :key="project._path">
+            <li
+                v-for="project in projects"
+                :key="project._path"
+                class="project-item relative group"
+            >
+                <div
+                    class="hover-indicator absolute left-[-40px] top-1/2 transform -translate-y-1/2 text-5xl font-bold opacity-0 group-hover:opacity-100 transition-opacity duration-700 ease-in-out pointer-events-none"
+                >
+                    <Icon name="right-arrow" />
+                </div>
                 <NuxtLink
                     :to="project._path"
                     class="w-full h-fit flex flex-col"
