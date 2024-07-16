b1cb26d3ac84127f68555fc58b4852c746a97801
Arturo Munoz <munoz.arturoroman@gmail.com>
2024-07-16 10:15:18 -0400

    build: add scroll to top button

---
+++ components/scroll-to-top.vue
@@ -0,0 +1,40 @@
+<template>
+    <div
+        v-if="mounted"
+        class="fixed bottom-24 right-32 transition-opacity duration-700 ease-in-out"
+        :class="{
+            'opacity-0 pointer-events-none': !showButton,
+            'opacity-100': showButton,
+        }"
+    >
+        <button @click="scrollToTop">
+            <Icon name="arrow-up" class="w-6 h-6" />
+        </button>
+    </div>
+</template>
+
+<script setup lang="ts">
+const showButton = ref(false);
+const mounted = ref(false);
+const scrollThreshold = 400;
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    });
+}
+
+function handleScroll() {
+    showButton.value = window.scrollY > scrollThreshold;
+}
+
+onMounted(() => {
+    window.addEventListener("scroll", handleScroll);
+    mounted.value = true;
+});
+
+onUnmounted(() => {
+    window.removeEventListener("scroll", handleScroll);
+});
+</script>
--- pages/projects/[slug].vue
+++ pages/projects/[slug].vue
@@ -1,6 +1,7 @@
 <template>
     <article v-if="page" class="w-full px-4 max-w-[768px] pt-8">
         <!-- <TableOfContents :page="page" /> -->
+        <ScrollToTop />
         <ArticleHead :page="page" />
         <ContentRenderer :value="page" class="space-y-6 pb-16">
             <template #empty>
