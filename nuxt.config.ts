8bf40a24fb7fabea58cf8cb95c8c6c4edec95f73
Arturo Munoz <munoz.arturoroman@gmail.com>
2024-07-29 15:58:53 -0400

    build: update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,43 +1,50 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    devtools: { enabled: true },
-    content: {
-        highlight: {
-            theme: {
-                default: "github-light-default",
-                "theme-paper": "solarized-light",
-                "theme-dark": "monokai",
-                "theme-ocean": "solarized-dark",
-                "theme-desert": "vesper",
-                "theme-forest": "slack-dark",
-            },
-            langs: [
-                "python",
-                "typescript",
-                "javascript",
-                "json",
-                "rust",
-                "bash",
-            ],
-        },
-    },
-    components: [
-        {
-            path: "~/components",
-            pathPrefix: false,
-        },
-    ],
-    classInject: {
-        fallback: ["theme-dark", "font-sans", "size-md"],
-    },
-    css: ["~/assets/css/scrollbar.css", "~/assets/css/themes.css"],
-    plugins: [],
-    modules: [
-        "@nuxtjs/tailwindcss",
-        "@nuxt/content",
-        "@nuxt/image",
-        "@pinia/nuxt",
-        "@pinia-plugin-persistedstate/nuxt",
-        "nuxt-class-inject",
-    ],
+  devtools: { enabled: true },
+
+  content: {
+      highlight: {
+          theme: {
+              default: "github-light-default",
+              "theme-paper": "solarized-light",
+              "theme-dark": "monokai",
+              "theme-ocean": "solarized-dark",
+              "theme-desert": "vesper",
+              "theme-forest": "slack-dark",
+          },
+          langs: [
+              "python",
+              "typescript",
+              "javascript",
+              "json",
+              "rust",
+              "bash",
+          ],
+      },
+  },
+
+  components: [
+      {
+          path: "~/components",
+          pathPrefix: false,
+      },
+  ],
+
+  classInject: {
+      fallback: ["theme-dark", "font-sans", "size-md"],
+  },
+
+  css: ["~/assets/css/scrollbar.css", "~/assets/css/themes.css"],
+  plugins: [],
+
+  modules: [
+      "@nuxtjs/tailwindcss",
+      "@nuxt/content",
+      "@nuxt/image",
+      "@pinia/nuxt",
+      "@pinia-plugin-persistedstate/nuxt",
+      "nuxt-class-inject",
+  ],
+
+  compatibilityDate: "2024-07-29",
 });
