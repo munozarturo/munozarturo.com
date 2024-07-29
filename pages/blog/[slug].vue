2c9407f52ce202672646b96d2625ab6254b802a9
Arturo Munoz <munoz.arturoroman@gmail.com>
2024-07-29 16:04:03 -0400

    build: add blog

---
+++ pages/blog/[slug].vue
@@ -0,0 +1,23 @@
+<template>
+    <article v-if="page" class="w-full px-4 max-w-[768px] pt-8">
+        <!-- <TableOfContents :page="page" /> -->
+        <ScrollToTop />
+        <ArticleHead :page="page" />
+        <ContentRenderer :value="page" class="space-y-6 pb-16">
+            <template #empty>
+                <p>No content found.</p>
+            </template>
+        </ContentRenderer>
+    </article>
+</template>
+
+<script setup lang="ts">
+const { path } = useRoute();
+const { data: page } = await useAsyncData(`content-${path}`, () =>
+    queryContent().where({ _path: path }).findOne()
+);
+
+useHead({
+    title: `${page.value?.title}`,
+});
+</script>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,11 +1,31 @@
 <template>
-    <div class="w-1/2 flex flex-col items-start justify-center space-y-4 pt-60">
-        <h1
-            class="flex flex-row items-center justify-center text-5xl font-bold"
-        >
-            i'm still working on this bit.
-        </h1>
-        <div class="text-2xl pl-2">hopefully i'll be done soon.</div>
+    <ScrollToTop />
+    <div class="w-full px-4 max-w-[768px] flex flex-col py-8">
+        <ul class="space-y-6">
+            <li
+                v-for="blog in blog"
+                :key="blog._path"
+                class="project-item relative group"
+            >
+                <div
+                    class="hover-indicator absolute left-[-40px] top-1/2 transform -translate-y-1/2 text-5xl font-bold opacity-0 group-hover:opacity-100 transition-opacity duration-700 ease-in-out pointer-events-none"
+                >
+                    <Icon name="right-arrow" />
+                </div>
+                <NuxtLink :to="blog._path" class="w-full h-fit flex flex-col">
+                    <div class="flex flex-row items-end justify-between">
+                        <h1 class="text-4xl font-bold">{{ blog.title }}</h1>
+                    </div>
+                    <p>{{ formatDate(new Date(blog.date)) }}</p>
+                    <h2 class="text-2xl font-bold text-highlight">
+                        {{ blog.subtitle }}
+                    </h2>
+                    <div class="flex fex-row space-x-2 text-highlight">
+                        <p v-for="tag in blog.tags">#{{ tag }}</p>
+                    </div>
+                </NuxtLink>
+            </li>
+        </ul>
     </div>
 </template>
 
@@ -13,4 +33,8 @@
 useHead({
     title: "blog",
 });
+
+const { data: blog } = await useAsyncData("blog", () =>
+    queryContent("blog").sort({ date: -1 }).find()
+);
 </script>
