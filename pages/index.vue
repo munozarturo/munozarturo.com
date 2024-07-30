09bf02598a0b4d2cb024536c320bf69e269384f9
Arturo Munoz <munoz.arturoroman@gmail.com>
2024-07-30 14:43:15 -0400

    build: make home page responsive

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,13 @@
 <template>
-    <div class="w-1/2 flex flex-col items-start justify-center space-y-4 pt-60">
+    <div
+        class="w-full px-4 sm:w-1/2 flex flex-col items-start justify-center space-y-4 pt-40 sm:pt-60"
+    >
         <h1
-            class="flex flex-row items-center justify-center text-5xl font-bold"
+            class="flex flex-row items-center justify-center text-4xl sm:text-5xl font-bold"
         >
             hello.
         </h1>
-        <div class="text-2xl pl-2">
+        <div class="text-xl md:first-letter:text-2xl pl-2">
             My name is Arturo Muñoz, I am a college student completing my
             Bachelor's Degree in Computer Science and this is a digital catalog
             of <a href="/me" class="font-semibold underline">me</a>, my
