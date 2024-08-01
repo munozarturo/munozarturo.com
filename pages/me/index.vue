781a6215fd0befa6641ef68abf61453ea48ba10b
Arturo Munoz <munoz.arturoroman@gmail.com>
2024-08-01 12:52:34 -0400

    build: /me responsive

--- pages/me/index.vue
+++ pages/me/index.vue
@@ -1,11 +1,13 @@
 <template>
-    <div class="w-1/2 flex flex-col items-start justify-center space-y-4 pt-60">
+    <div
+        class="px-4 sm:w-2/3 pt-[calc(30vh)] sm:h-[calc(100vh-4rem)] flex flex-col items-start gap-2"
+    >
         <h1
-            class="flex flex-row items-center justify-center text-5xl font-bold"
+            class="flex flex-row items-center justify-center text-4xl sm:text-5xl font-bold"
         >
             i'm still working on this bit.
         </h1>
-        <div class="text-2xl pl-2">hopefully i'll be done soon.</div>
+        <div class="text-2xl pl-2">hopefully done soon...</div>
     </div>
 </template>
 
