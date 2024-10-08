ff42b805e7f17aad3123308ece383dc035c3348d
Arturo Munoz <munoz.arturoroman@gmail.com>
2024-10-08 15:08:54 -0400

    build: reverse order for skills first

--- pages/me/resume.vue
+++ pages/me/resume.vue
@@ -86,26 +86,6 @@
             </section>
 
             <section>
-                <h2 class="text-lg sm:text-xl font-bold">Experience</h2>
-                <hr class="border-t border-highlight" />
-                <div v-for="exp in resume.experience">
-                    <span class="flex flex-col sm:flex-row justify-between">
-                        <h3 class="font-bold">{{ exp.company }}</h3>
-                        <p class="font-bold">{{ exp.dates }}</p></span
-                    >
-                    <span class="flex flex-col sm:flex-row justify-between">
-                        <p class="italic">{{ exp.title }}</p>
-                        <p>{{ exp.location }}</p>
-                    </span>
-                    <ul class="list-disc pl-4 sm:pl-6 md:pl-8">
-                        <li v-for="responsibility in exp.responsibilities">
-                            {{ responsibility }}
-                        </li>
-                    </ul>
-                </div>
-            </section>
-
-            <section>
                 <h2 class="text-lg sm:text-xl font-bold">Skills</h2>
                 <hr class="border-t border-highlight" />
                 <ul class="pl-4 sm:pl-6 md:pl-8">
@@ -139,6 +119,26 @@
                         </p>
                     </li>
                 </ul>
+            </section>
+
+            <section>
+                <h2 class="text-lg sm:text-xl font-bold">Experience</h2>
+                <hr class="border-t border-highlight" />
+                <div v-for="exp in resume.experience">
+                    <span class="flex flex-col sm:flex-row justify-between">
+                        <h3 class="font-bold">{{ exp.company }}</h3>
+                        <p class="font-bold">{{ exp.dates }}</p></span
+                    >
+                    <span class="flex flex-col sm:flex-row justify-between">
+                        <p class="italic">{{ exp.title }}</p>
+                        <p>{{ exp.location }}</p>
+                    </span>
+                    <ul class="list-disc pl-4 sm:pl-6 md:pl-8">
+                        <li v-for="responsibility in exp.responsibilities">
+                            {{ responsibility }}
+                        </li>
+                    </ul>
+                </div>
             </section>
 
             <section>
